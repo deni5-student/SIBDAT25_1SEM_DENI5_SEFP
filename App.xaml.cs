0e53f040790eafef6288e48708bd29f1cb3c9ee9
deni5-student <deni5@student.eadania.dk>
2025-12-17 00:44:36 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SIBDAT25_1SEM_DENI5_SEFP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
